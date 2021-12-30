ed595c3a1b5b6fa6c96db30651b54177712ce383
dreamscached <dreamscache.d@gmail.com>
2021-12-30 15:09:52 +0100

    Improvements

--- configure.sh
+++ configure.sh
@@ -5,17 +5,39 @@ prompt() {
     printf " [%s]" "$3"
   fi
   printf "%s " ":"
+
   read -r "$1"
   if [ -z "$(eval printf "%s" '$'"$1")" ]; then
     eval "$1"=\""$(printf "%s" "$3" | sed 's/\\/\\\\/' | sed 's/"/\\"/')"\"
   fi
 }
 
-prompt package     "Enter your plugin package"     "com.example"
-prompt name        "Enter your plugin name"        "$(basename "$(readlink -f "$(dirname -- "$0")")")"
-prompt version     "Enter your plugin version"     "1.0.0"
+question() {
+  printf "%s" "$2"
+  if [ "$#" -gt 2 ]; then
+    printf " [%s]" "$3"
+  fi
+  printf "%s " "?"
+
+  read -r "$1"
+  if [ -z "$(eval printf "%s" '$'"$1")" ]; then
+    eval "$1"=\""$(printf "%s" "$3" | sed 's/\\/\\\\/' | sed 's/"/\\"/')"\"
+  fi
+
+  if [ "$1" = "y" ] || [ "$1" = "yes" ]; then
+    # shellcheck disable=SC2140
+    eval "$1"="1"
+  fi
+}
+
+prompt package "Enter your plugin package" "com.example"
+prompt name "Enter your plugin name" "$(basename "$(readlink -f "$(dirname -- "$0")")")"
+prompt version "Enter your plugin version" "1.0.0"
 prompt description "Enter your plugin description" "A clear and concise description of a Spigot plugin."
-prompt author      "Enter your plugin author name" "$(whoami)"
+prompt author "Enter your plugin author name" "$(whoami)"
+
+question git "Do you want to create a Git repository?" "yes"
+question annotations "Do you want to add JetBrains Annotations?" "yes"
 
 # shellcheck disable=SC2154
 package_path="$(printf "%s" "$package" | sed 's/\./\//')"
@@ -27,12 +49,21 @@ sed -i 's/group = "com\.example"/group = "'"$package"'"/' "build.gradle.kts"
 # shellcheck disable=SC2154
 sed -i 's/description = "A clear and concise description of a Spigot plugin\."/description = "'"$description"'"/' "build.gradle.kts"
 sed -i 's/prefix = "com.example.myawesomeplugin.dependency"/prefix = "'"$package"'.'"$package_name"'.dependency"/' "build.gradle.kts"
+# shellcheck disable=SC2154
+if [ "$annotations" != "1" ]; then
+  sed -i '/    compileOnly("org.jetbrains", "annotations", "23.0.0")/d' "build.gradle.kts"
+fi
 
 # Rename package and main class
 mkdir -p "src/main/java/$package_path"
 mv "src/main/java/com/example/myawesomeplugin" "src/main/java/$package_path/$package_name"
 mv "src/main/java/$package_path/$package_name/MyAwesomePlugin.java" "src/main/java/$package_path/$package_name/$name.java"
-rm -rf "src/main/java/com"
+if [ "$package_path" != "com/example" ]; then
+  rm -d "src/main/java/com/example"
+fi
+if [ "${package_path%%/*}" != "com" ]; then
+  rm -d "src/main/java/com"
+fi
 
 # Configure main class file
 sed -i 's/public final class MyAwesomePlugin extends JavaPlugin {/public final class '"$name"' extends JavaPlugin {/' "src/main/java/$package_path/$package_name/$name.java"
@@ -56,8 +87,11 @@ rm -f -- "$0" "README.md" "LICENSE"
 
 # Create git repository and create initial commit
 rm -rf ".git"
-git init >"/dev/null"
-git add "." >"/dev/null"
-git commit -m "Initial commit" >"/dev/null"
+# shellcheck disable=SC2154
+if [ "$git" = 1 ]; then
+  git init >"/dev/null"
+  git add "." >"/dev/null"
+  git commit -m "Initial commit" >"/dev/null"
+fi
 
 echo "Template project configured and is ready for use!"
